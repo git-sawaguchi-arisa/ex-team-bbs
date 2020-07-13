733754d23cdf78852fcec23cd688dd882fa426c7
sawaguchiarisa <arisa.sawaguchi@rakus-partners.co.jp>
2020-07-13 11:08:10 +0900

    CommentForm作成

---
+++ src/main/java/com/example/form/CommentForm.java
@@ -0,0 +1,44 @@
+package com.example.form;
+
+public class CommentForm {
+
+	public Integer id;
+	
+	public String name;
+	
+	public String content;
+	
+	public Integer articleId;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getArticleId() {
+		return articleId;
+	}
+
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	} 
+}
